1f821f9139dc9024860941671c55cd881096a689
Carl Caum <carl@carlcaum.com>
2011-10-18 17:36:04 -0700

    Overhaul of retry_action method
    Rewrite of the retry_action method and refactor of the create_tags and ssh_connect methods

--- lib/puppet/cloudpack/utils.rb
+++ lib/puppet/cloudpack/utils.rb
@@ -1,22 +1,51 @@
 module Puppet::CloudPack::Utils
   class RetryException < Exception
+
+    class RetryException::NoBlockGiven < RetryException
+    end
+
+    class RetryException::Timeout < RetryException
+    end
+
+    class RetryException::OutOfRetries < RetryException
+    end
   end
   
-  def self.retry_action(retries=3, pause=0)
+  def self.retry_action( parameters = { :retry_exceptions => Hash.new, :timeout => nil } )
     # Helper method to retry actions n number of times, re-raise the exception
     # after the retry count has been met.
     unless block_given?
-      raise RetryException, 'No block given'
+      raise RetryException::NoBlockGiven
     end
+
+    start = Time.now
+    failures = 0
+
     begin
       yield
-    rescue 
-      sleep pause if pause > 0
-      if (retries -= 1) > 0
-        retry
-      else 
-        raise
+    rescue Exception => e 
+      #If we were giving exceptions to catch,
+      #catch the excptions we care about and retry.
+      #All others fail hard
+      if (not parameters[:retry_exceptions].keys.empty?) and parameters[:retry_exceptions].keys.include?(e)
+
+        #If the max run time was specifed and we haven't run out
+        unless parameters[:timeout].nil? and ( (Time.now - start) < parameters[:timeout] )
+          raise RetryException::Timeout 
+        end
+          
+        Puppet.info("Caught exception #{e}")
+        Puppet.info(parameters[:exception][e])
+      elsif (not parameters[:retry_exceptions].keys.empty?)
+        raise e
       end
+
+      failures += 1
+      
+      sleep (((2 ** failures) -1) * 0.1)
+
+      retry
+
     end
   end
 end
